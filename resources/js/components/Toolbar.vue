e0db7dbea45fe1f41eed59828da7ddeed8250559
thinhnx_fimplus <thinhnx@fimplus.vn>
2020-04-06 16:31:47 +0700

    Login component

--- resources/js/components/Toolbar.vue
+++ resources/js/components/Toolbar.vue
@@ -2,12 +2,16 @@
   <v-toolbar>
     <v-toolbar-title>Lucifer</v-toolbar-title>
     <v-spacer></v-spacer>
-    <v-toolbar-items>
-      <v-btn text>Forum</v-btn>
+    <div>
+      <router-link to="/forum">
+        <v-btn text>Forum</v-btn>
+      </router-link>
       <v-btn text>Ask Question</v-btn>
       <v-btn text>Category</v-btn>
-      <v-btn text>Login</v-btn>
-    </v-toolbar-items>
+      <router-link to="/login">
+        <v-btn text>Login</v-btn>
+      </router-link>
+    </div>
   </v-toolbar>
 </template>
 <script>
