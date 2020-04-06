e5e9a4cc6a6a87626043c19c2b17d5288e29c2df
thinhnx_fimplus <thinhnx@fimplus.vn>
2020-04-06 15:54:47 +0700

    Vuetify, VueRouter and Initial Components

---
+++ resources/js/components/AppFooter.vue
@@ -0,0 +1,12 @@
+<template>
+  <v-footer class="mt-3">
+    Lucifer
+    <v-spacer></v-spacer>
+    <div>&copy; {{ new Date().getFullYear() }}</div>
+  </v-footer>
+</template>
+<script>
+export default {};
+</script>
+<style lang="">
+</style>
---
+++ resources/js/components/Toolbar.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-toolbar>
+    <v-toolbar-title>Lucifer</v-toolbar-title>
+    <v-spacer></v-spacer>
+    <v-toolbar-items>
+      <v-btn text>Forum</v-btn>
+      <v-btn text>Ask Question</v-btn>
+      <v-btn text>Category</v-btn>
+      <v-btn text>Login</v-btn>
+    </v-toolbar-items>
+  </v-toolbar>
+</template>
+<script>
+export default {};
+</script>
+<style lang="">
+</style>
