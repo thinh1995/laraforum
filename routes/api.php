cdfc190208593ee2a0f6623027750219948c55d8
thinhnx_fimplus <thinhnx@fimplus.vn>
2020-04-06 13:38:41 +0700

    JWT Package

--- routes/api.php
+++ routes/api.php
@@ -5,3 +5,17 @@ Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/question/{question}/reply', 'ReplyController');
 Route::post('/like/{reply}', 'LikeControler@likeIt');
 Route::delete('/like/{reply}', 'LikeControler@unLikeIt');
+
+Route::group([
+
+  'middleware' => 'api',
+  'prefix' => 'auth'
+
+], function ($router) {
+
+  Route::post('login', 'AuthController@login');
+  Route::post('logout', 'AuthController@logout');
+  Route::post('refresh', 'AuthController@refresh');
+  Route::post('me', 'AuthController@me');
+
+});
