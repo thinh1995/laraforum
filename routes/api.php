2e859264c2b35e6d28db7ef3878e3adade00ac6f
thinhnx_fimplus <thinhnx@fimplus.vn>
2020-04-06 11:52:15 +0700

    Like and Dislike system

--- routes/api.php
+++ routes/api.php
@@ -3,3 +3,5 @@
 Route::apiResource('/question', 'QuestionController');
 Route::apiResource('/category', 'CategoryController');
 Route::apiResource('/question/{question}/reply', 'ReplyController');
+Route::post('/like/{reply}', 'LikeControler@likeIt');
+Route::delete('/like/{reply}', 'LikeControler@unLikeIt');
